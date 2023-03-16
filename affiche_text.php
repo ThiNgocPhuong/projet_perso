1f788111b3933fe25244a1d0c38ee3797000a472
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2023-03-16 11:47:45 +0100

    Add files via upload

--- affiche_text.php
+++ affiche_text.php
@@ -1,13 +1,45 @@
 <?php
-    //connexion à la database
-    $user="Caroline"
-    $pass="pingu"
+// Connexion à la base de données
+$servername = "localhost";
+$username = "Caroline";
+$password = "pingu";
+$dbname = "projet_perso";
 
-    try{
-        $objPDO = new PDO('mysql:host=localhost;dbname=projet_perso', $user, $pass);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-        //préparation de la requête
-        $request = $objPDO -> prepare('SELECT text from chapitre WHERE id like 1 ');
-        
-    }
+// Vérification de la connexion
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Requête SQL pour récupérer les données
+$sql = "SELECT text FROM chapitre WHERE id LIKE 1";
+$result = $conn->query($sql);
 ?>
+
+<html>
+    <header>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Histoire</title>
+    </header>
+    <body>
+        <h1>Le commencement</h1>
+        <?php
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    echo "<br><br>".$row["text"]."<br><br>";
+                }
+                echo "</table>";
+            } else {
+                echo "Aucun résultat trouvé.";
+            }
+
+            // Fermeture de la connexion
+            $conn->close();
+        ?>
+    </body>
+</html>
+
+
