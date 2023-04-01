006f7c0b701184f2019a7b358f3864c1f9ad51c9
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2023-04-01 15:29:21 +0200

    Add files via upload

--- inscription.php
+++ inscription.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/form.css">
     <title>Inscription</title>
 </head>
 <body>
@@ -19,24 +20,25 @@
         </nav>
     </header>
     <br>
+    <h1> Inscription </h1>
     <form action="new_lecteur.php" method="post" enctype="multipart/form-data">
-        <div class="contain" style="margin: 150px 254px 65px 185px; ">
+        <div class="contain">
             <div class="input_email">
                 <label for="email">Adresse Mail</label><br>
                 <input type="text" name="email" id="email" placeholder="Veuillez saisir votre email" pattern='(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))' required>
             </div>
             <div class="input_pseudo">
                 <label for="pseudo">Pseudo</label><br>
-                <input type="text" name="pseudo" id="pseudo" placeholder="Veuillez saisir votre pseudo" pattern='/^[a-zA-Z0-9_.+-]{3,20}$/'>
+                <input type="text" name="pseudo" id="pseudo" placeholder="Veuillez saisir votre pseudo" >
             </div>
             <div class="input_mdp">
                 <label for="mdp">Mot de passe</label><br>
-                <input type="password" name="mdp" id="mdp" placeholder="Veuillez saisir votre mot de passe"  pattern='/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/'>
-                <p>Pour que votre mot de passe soit accepté, il doit comporter au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial. De plus, sa longueur doit être d'au moins 8 caractères.</p>
+                <input type="password" name="mdp" id="mdp" placeholder="Veuillez saisir votre mot de passe" >
             </div>
             <div id="input_confirm_mdp" >
                 <label for="confirm_mdp">Confirmation du mot de passe</label><br>
                 <input type="password" name="confirm_mdp" onchange="validate()" id="confirm_mdp" placeholder="Veuillez ressaisir votre mot de passe" required>
+                <p>Pour que votre mot de passe soit accepté, il doit comporter au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial. De plus, sa longueur doit être d'au moins 8 caractères.</p>
             </div>
             <br>
             <div id="bouton" >
